6d5e049415451473dec54d3ee90c747736c68c71
Arjun-0000 <bboyarjun07@gmail.com>
2022-03-16 07:57:05 +0545

    Added Json API data fetch -

--- src/components/Youtube.js
+++ src/components/Youtube.js
@@ -1,8 +1,41 @@
 import React from 'react';
 import 'animate.css';
 import { Accordion } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function Youtube() {
+/* defining how our Array Object show be look like */
+const filmsArray = [
+	{
+		name: "first"
+	}
+]
+
+const Youtube = () => {
+
+	const [films, setFilms] = useState(filmsArray);
+
+	useEffect(() => {
+		axios.get('https://reqres.in/api/unknown')
+			.then(res => {
+
+				let filmData = res.data.data;  // Json API bata aako value yesmah hunxa, jun chai filmsArray mah direct rakhna mildain, kinaki data types mildaina 
+
+				//so yo tala ko chai shirf JSON file ko Name single single gari Hamro Films mah rakhneko matra ho, bro
+				for (var i = 0; i<filmData.length; i++){
+					setFilms((prevFilms) => [
+						...prevFilms,
+						{
+							name: filmData[i].name,
+						},
+					]);
+				};
+			})  /* just error handling - doont edit down this part */
+			.catch(err => {
+				console.log(err);
+			})
+	}, []);
+
 	return (
 		<div className="d-flex">
 			<iframe
@@ -16,70 +49,52 @@ function Youtube() {
 				<Accordion.Item eventKey="0">
 					<Accordion.Header>Fantasy</Accordion.Header>
 					<Accordion.Body>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-						ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-						aliquip ex ea commodo consequat. Duis aute irure dolor in
-						reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-						pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-						culpa qui officia deserunt mollit anim id est laborum.
+						{/* yo chai bro k gareko bhene, films array (list) lai single single gari haleko . direct sab halna mildaina*/ }
+						{films.map((film, index) =>(
+							<span key={index}>{film.name} + </span>
+						))}
 						<a href="#Fantasy">Go to</a>
 					</Accordion.Body>
 				</Accordion.Item>
 				<Accordion.Item eventKey="1">
 					<Accordion.Header>Sci-Fi</Accordion.Header>
 					<Accordion.Body>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-						ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-						aliquip ex ea commodo consequat. Duis aute irure dolor in
-						reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-						pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-						culpa qui officia deserunt mollit anim id est laborum.
+						{films.map((film, index) =>(
+							<span key={index}>Name: {film.name}</span>
+						))}
 						<a href="#Scifi">Go to</a>
 					</Accordion.Body>
 				</Accordion.Item>
 				<Accordion.Item eventKey="2">
 					<Accordion.Header>Mystery</Accordion.Header>
 					<Accordion.Body>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-						ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-						aliquip ex ea commodo consequat. Duis aute irure dolor in
-						reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-						pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-						culpa qui officia deserunt mollit anim id est laborum.
+						{films.map((film, index) =>(
+							<span key={index}>Name: {film.name}</span>
+						))}
 						<a href="#Mystery">Go to</a>
 					</Accordion.Body>
 				</Accordion.Item>
 				<Accordion.Item eventKey="3">
 					<Accordion.Header>Thriller</Accordion.Header>
 					<Accordion.Body>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-						ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-						aliquip ex ea commodo consequat. Duis aute irure dolor in
-						reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-						pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-						culpa qui officia deserunt mollit anim id est laborum.
+						{films.map((film, index) =>(
+							<span key={index}>Name: {film.name}</span>
+						))}
 						<a href="#Thriller">Go to</a>
 					</Accordion.Body>
 				</Accordion.Item>
 				<Accordion.Item eventKey="4">
 					<Accordion.Header>Romance</Accordion.Header>
 					<Accordion.Body>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-						ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-						aliquip ex ea commodo consequat. Duis aute irure dolor in
-						reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-						pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-						culpa qui officia deserunt mollit anim id est laborum.
+						{films.map((film, index) =>(
+							<span key={index}>Name: {film.name}</span>
+						))}
 						<a href="#Romance">Go to</a>
 					</Accordion.Body>
 				</Accordion.Item>
 			</Accordion>
 		</div>
+			
 	);
 }
 
